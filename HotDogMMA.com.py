00fb9005373a10373e9a1807f00b579d66704b97
daniel0ceanCA <danielgojeda.1@gmail.com>
2025-02-03 23:25:41 -0800

    HOTDOGFIGHTING - New News Source

--- HotDogMMA.com.py
+++ HotDogMMA.com.py
@@ -36,8 +36,9 @@ NEWS_SOURCES = {
         'https://www.lowkickmma.com/feed/',
         'https://www.mmamania.com/rss/index.xml',
         'https://www.ufc.com/rss/news',
-        'https://www.mmafighting.com/rss'
-        'https://www.mmaweekly.com/feed'
+        'https://www.mmafighting.com/rss',
+        'https://www.mmaweekly.com/feed',
+        'https://mmajunkie.usatoday.com/feed',
         #MMAFEEDS
     ],
     'Boxing': [
